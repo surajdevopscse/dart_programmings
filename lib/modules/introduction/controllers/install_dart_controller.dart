53475232e5eba66e76c8d7135200bbd75c5ccd3c
Suraj Yadav <suraj.yadav@hostbooks.com>
2023-11-25 13:15:32 +0530

    Install Dart Page Done

--- lib/modules/introduction/controllers/install_dart_controller.dart
+++ lib/modules/introduction/controllers/install_dart_controller.dart
@@ -1,10 +1,129 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class InstallDartController extends GetxController{
-    bool showBackToTopButton = false;
+class InstallDartController extends GetxController {
+  bool showBackToTopButton = false;
   late ScrollController scrollController;
-   @override
+
+  String dartInstall =
+      'There are multiple ways to install a dart on your system. You can install Dart on Windows, Mac, and Linux or run it from the browser.\nTo install Dart, you can follow these general steps. Keep in mind that specific instructions might vary based on your operating system.';
+  String dartCheckVersion =
+      'Open your command prompt and type dart --version. The dart is successfully installed on your system if it gives you a version code. If not, watch the video above.';
+  String dartPadWebTool =
+      'You can run the dart program on your browser without installing any software. Dartpad is a web tool to write and run your dart code.';
+
+  final Uri dartPadUrl = Uri.parse('https://dartpad.dev/');
+  List<Map<String, dynamic>> dartInstallationStepsWindow = [
+    {
+      'title': 'Download Dart SDK:',
+      'substeps': [
+        'Visit the Dart SDK page on the Dart website: Dart SDK.',
+        'Click on the Windows tab to download the Dart SDK for Windows.',
+        'Follow the installation instructions provided on the Dart website.'
+      ]
+    },
+    {
+      'title': 'Add Dart to System Path:',
+      'substeps': [
+        "After installation, you need to add the Dart SDK to your system's PATH.",
+        'Search for "Environment Variables" in the Windows Start menu and open it.',
+        'Under the "System variables" section, find and select the "Path" variable, then click "Edit".',
+        'Click "New" and add the path to the Dart SDK\'s bin directory. The default path is often C:\\dart\\dart-sdk\\bin (adjust accordingly).'
+      ]
+    },
+    {
+      'title': 'Verify Installation:',
+      'substeps': [
+        'Open a new command prompt or terminal window.',
+        'Type "dart --version" and press Enter. This should display the Dart version if the installation was successful.'
+      ]
+    }
+  ];
+
+  List<Map<String, dynamic>> installationStepsMac = [
+    {
+      'title': 'Install Homebrew (if not already installed):',
+      'substeps': [
+        'Open Terminal.',
+        'Run the following command to install Homebrew:',
+        '/bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+      ]
+    },
+    {
+      'title': 'Install Dart SDK using Homebrew:',
+      'substeps': [
+        'Run the following command to install Dart:',
+        'brew tap dart-lang/dart',
+        'brew install dart'
+      ]
+    },
+    {
+      'title': 'Verify Installation:',
+      'substeps': [
+        'Open a new Terminal window.',
+        "Type 'dart --version' and press Enter. This should display the Dart version if the installation was successful."
+      ]
+    },
+  ];
+
+  List<Map<String, dynamic>> dartInstallationStepsLinux = [
+    {
+      'title': 'Download Dart SDK:',
+      'substeps': [
+        'Visit the Dart SDK page on the Dart website: Dart SDK.',
+        'Click on the Linux tab to download the Dart SDK for Linux.',
+        'Follow the installation instructions provided on the Dart website.'
+      ]
+    },
+    {
+      'title': 'Add Dart to System Path:',
+      'substeps': [
+        "After installation, you need to add the Dart SDK to your system's PATH.",
+        'Open your shell profile file (e.g., ~/.bashrc, ~/.zshrc) in a text editor.',
+        'Add the following line at the end of the file:',
+        'export PATH="\$PATH:/path/to/dart-sdk/bin"',
+        'Replace /path/to/dart-sdk with the actual path where Dart is installed.'
+      ]
+    },
+    {
+      'title': 'Verify Installation:',
+      'substeps': [
+        'Open a new terminal window.',
+        'Type dart --version and press Enter. This should display the Dart version if the installation was successful.'
+      ]
+    },
+  ];
+  final List<Map<String, String>> dartHelpfullCommands = [
+    {'Command': 'dart --help', 'Description': 'Show all available commands.'},
+    {'Command': 'dart --version', 'Description': 'Version check.'},
+    {'Command': 'dart filename.dart', 'Description': 'Run the dart file.'},
+    {'Command': 'dart create', 'Description': 'Create a dart project.'},
+    {
+      'Command': 'dart fix',
+      'Description': 'Update dart project to new syntax.'
+    },
+    {
+      'Command': 'dart compile exe bin/dart.dart',
+      'Description': 'Compile dart code.'
+    },
+    {
+      'Command': 'dart compile js bin/dart.dart',
+      'Description':
+          'Compile dart to javascript. You can run this file with Node.js.'
+    },
+  ];
+
+  Future<void> launchDartPad() async {
+    if (!await launchUrl(
+      dartPadUrl,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw 'Could not launch $dartPadUrl';
+    }
+  }
+
+  @override
   void onInit() async {
     scrollController = ScrollController()
       ..addListener(() {
--- lib/modules/introduction/pages/install_dart.dart
+++ lib/modules/introduction/pages/install_dart.dart
@@ -1,6 +1,9 @@
+import 'package:dart_programing/app/common_widgets/big_text.dart';
+import 'package:dart_programing/app/common_widgets/common_height.dart';
 import 'package:dart_programing/app/common_widgets/page_header.dart';
 import 'package:dart_programing/modules/introduction/controllers/install_dart_controller.dart';
 import 'package:dart_programing/utils/constants/colors.dart';
+import 'package:dart_programing/utils/constants/common_text_style/app_style.dart';
 import 'package:dart_programing/utils/constants/strings/string_keys.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -27,6 +30,298 @@ class InstallDart extends GetView<InstallDartController> {
                   headerName: SK.installDart,
                 ),
                 backgroundColor: C.primary50,
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: BigText(
+                          text: 'INSTALL DART',
+                          style: AppStyle.globalBigTextStyle.copyWith(
+                            fontSize: 40,
+                            letterSpacing: 1.8,
+                            height: 2.5,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      BigText(
+                        text: 'Dart Installation : ',
+                        style: AppStyle.globalBigTextStyle.copyWith(
+                          fontSize: 24,
+                          letterSpacing: 1.8,
+                          height: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      BigText(
+                        text: controller.dartInstall,
+                        style: AppStyle.globalSmallTextStyle.copyWith(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      BigText(
+                        text: 'Dart Window Installation: ',
+                        style: AppStyle.globalBigTextStyle.copyWith(
+                          fontSize: 24,
+                          letterSpacing: 1.8,
+                          height: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (var step
+                              in controller.dartInstallationStepsWindow)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  step['title'],
+                                  style: AppStyle.globalBigTextStyle.copyWith(
+                                    fontSize: 20,
+                                    letterSpacing: 1.8,
+                                    height: 2.5,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                for (var substep in step['substeps'])
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 16.0),
+                                    child: Text(
+                                      '• $substep',
+                                      style: AppStyle.globalSmallTextStyle
+                                          .copyWith(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                        ],
+                      ),
+                      const CommonHeight(
+                        height: 40,
+                      ),
+                      BigText(
+                        text: 'Dart MacOS Installation: ',
+                        style: AppStyle.globalBigTextStyle.copyWith(
+                          fontSize: 24,
+                          letterSpacing: 1.8,
+                          height: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (var step in controller.installationStepsMac)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  step['title'],
+                                  style: AppStyle.globalBigTextStyle.copyWith(
+                                    fontSize: 20,
+                                    letterSpacing: 1.8,
+                                    height: 2.5,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                for (var substep in step['substeps'])
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 16.0),
+                                    child: Text(
+                                      '• $substep',
+                                      style: AppStyle.globalSmallTextStyle
+                                          .copyWith(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                        ],
+                      ),
+                      const CommonHeight(
+                        height: 40,
+                      ),
+                      BigText(
+                        text: 'Dart Linux Installation: ',
+                        style: AppStyle.globalBigTextStyle.copyWith(
+                          fontSize: 24,
+                          letterSpacing: 1.8,
+                          height: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (var step
+                              in controller.dartInstallationStepsLinux)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  step['title'],
+                                  style: AppStyle.globalBigTextStyle.copyWith(
+                                    fontSize: 20,
+                                    letterSpacing: 1.8,
+                                    height: 2.5,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                for (var substep in step['substeps'])
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 16.0),
+                                    child: Text(
+                                      '• $substep',
+                                      style: AppStyle.globalSmallTextStyle
+                                          .copyWith(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                        ],
+                      ),
+                      BigText(
+                        text: 'Some Useful Commands in Dart: ',
+                        style: AppStyle.globalBigTextStyle.copyWith(
+                          fontSize: 24,
+                          letterSpacing: 1.8,
+                          height: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: DataTable(
+                          columns: [
+                            DataColumn(
+                                label: Text(
+                              'Command',
+                              style: AppStyle.globalBigTextStyle.copyWith(
+                                fontSize: 20,
+                                letterSpacing: 1.8,
+                                height: 2.5,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Description',
+                              style: AppStyle.globalBigTextStyle.copyWith(
+                                fontSize: 20,
+                                letterSpacing: 1.8,
+                                height: 2.5,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                          ],
+                          rows: controller.dartHelpfullCommands.map((command) {
+                            return DataRow(
+                              cells: [
+                                DataCell(Text(
+                                  command['Command'] ?? '',
+                                  style: AppStyle.globalBigTextStyle.copyWith(
+                                    fontSize: 16,
+                                    letterSpacing: 1.8,
+                                    height: 2.5,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                                DataCell(Text(
+                                  command['Description'] ?? '',
+                                  style: AppStyle.globalSmallTextStyle.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                )),
+                              ],
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      const CommonHeight(
+                        height: 40,
+                      ),
+                      BigText(
+                        text: 'Check Dart Installation: ',
+                        style: AppStyle.globalBigTextStyle.copyWith(
+                          fontSize: 24,
+                          letterSpacing: 1.8,
+                          height: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      BigText(
+                        text: controller.dartCheckVersion,
+                        style: AppStyle.globalSmallTextStyle.copyWith(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      const CommonHeight(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          BigText(
+                            text: 'Run Dart On Web: ',
+                            style: AppStyle.globalBigTextStyle.copyWith(
+                              fontSize: 24,
+                              letterSpacing: 1.8,
+                              height: 2.5,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () => controller.launchDartPad(),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'DartPad',
+                                  style: AppStyle.globalBigTextStyle.copyWith(
+                                    fontSize: 24,
+                                    color: AppColors.stepper,
+                                    letterSpacing: 1.8,
+                                    height: 2.5,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Icon(
+                                  Icons.open_in_browser,
+                                  color: AppColors.stepper,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      BigText(
+                        text: controller.dartPadWebTool,
+                        style: AppStyle.globalSmallTextStyle.copyWith(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      const CommonHeight(
+                        height: 80,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
