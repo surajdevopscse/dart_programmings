27f31fe1c8e137417cc3054beb36b560571eb12e
Suraj Yadav <suraj.yadav@hostbooks.com>
2024-04-27 14:32:07 +0530

    Update Route using Go Route

--- lib/app/views/side_menu/side_menu_view.dart
+++ lib/app/views/side_menu/side_menu_view.dart
@@ -1,5 +1,6 @@
 import 'package:dart_programing/app/views/side_menu/custom_expansion_panel.dart';
 import 'package:dart_programing/app/views/side_menu/nav_tile.dart';
+import 'package:dart_programing/utils/constants/colors.dart';
 import 'package:dart_programing/utils/constants/images.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -15,73 +16,89 @@ class SideMenuView extends GetView<SideMenuController> {
       body: GetBuilder<SideMenuController>(builder: (_) {
         return SizedBox(
           height: double.maxFinite,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Image.asset(I.leanrnDartLogo),
+          child: Column(
+            children: [
+              SizedBox(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: Image.asset(I.leanrnDartLogo),
+                    ),
+                    const Divider(
+                      indent: 16,
+                      endIndent: 16,
+                      height: 1,
+                      color: AppColors.purple600,
+                    ),
+                  ],
                 ),
-                const Divider(
-                  indent: 16,
-                  endIndent: 16,
-                  color: Colors.grey,
-                ),
-                CustomExpansionPanelList(
-                  dividerColor: Colors.white,
-                  headerMargin: const EdgeInsets.all(8),
-                  expandedHeaderMargin: const EdgeInsets.all(8),
-                  materialGapSize: 0,
-                  children: SideNavBarParentEnum.values.map(
-                    (parent) {
-                      final isSelected = controller.selectedParent == parent;
-                      return CustomExpansionPanel(
-                        canTapOnHeader: true,
-                        isExpanded: (isSelected && controller.isExpande),
-                        headerBuilder: (context, isExpanded) {
-                          return NavParentTile(
-                            title: parent.title,
-                            image:
-                                isSelected ? parent.image.$1 : parent.image.$2,
-                            isSelected: isSelected,
-                          );
-                        },
-                        body: Column(
-                          children: parent.children.map((child) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8),
-                              child: NavTileChild(
-                                onTap: () {
-                                  controller.navigate(
-                                      parent: parent, child: child);
-                                },
-                                isSelected: controller.selectedChild == child,
-                                title: child.title,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CustomExpansionPanelList(
+                        dividerColor: Colors.white,
+                        headerMargin: const EdgeInsets.all(8),
+                        expandedHeaderMargin: const EdgeInsets.all(8),
+                        materialGapSize: 0,
+                        children: SideNavBarParentEnum.values.map(
+                          (parent) {
+                            final isSelected =
+                                controller.selectedParent == parent;
+                            return CustomExpansionPanel(
+                              canTapOnHeader: true,
+                              isExpanded: (isSelected && controller.isExpande),
+                              headerBuilder: (context, isExpanded) {
+                                return NavParentTile(
+                                  title: parent.title,
+                                  image: isSelected
+                                      ? parent.image.$1
+                                      : parent.image.$2,
+                                  isSelected: isSelected,
+                                );
+                              },
+                              body: Column(
+                                children: parent.children.map((child) {
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8),
+                                    child: NavTileChild(
+                                      onTap: () {
+                                        controller.navigate(
+                                            parent: parent, child: child);
+                                      },
+                                      isSelected:
+                                          controller.selectedChild == child,
+                                      title: child.title,
+                                    ),
+                                  );
+                                }).toList(),
                               ),
                             );
-                          }).toList(),
-                        ),
-                      );
-                    },
-                  ).toList(),
-                  expansionCallback: (panelIndex, isExpanded) {
-                    final parent = SideNavBarParentEnum.values
-                        .firstWhere((element) => element.index == panelIndex);
+                          },
+                        ).toList(),
+                        expansionCallback: (panelIndex, isExpanded) {
+                          final parent = SideNavBarParentEnum.values.firstWhere(
+                              (element) => element.index == panelIndex);
 
-                    if (controller.selectedParent != parent) {
-                      controller.isExpande = isExpanded;
-                      controller.navigate(parent: parent);
-                    } else {
-                      controller.isExpande = isExpanded;
-                      controller.update();
-                    }
-                  },
+                          if (controller.selectedParent != parent) {
+                            controller.isExpande = isExpanded;
+                            controller.navigate(parent: parent);
+                          } else {
+                            controller.isExpande = isExpanded;
+                            controller.update();
+                          }
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       }),
